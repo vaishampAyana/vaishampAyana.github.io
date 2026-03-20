338dd052fa63b1c4ac85c9294de90125f22097c5
vaishampAyana <hackerbachcha420@gmail.com>
2026-03-20 09:54:38 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'pingala-chandas-v5';
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache =>
+      cache.addAll(['/manifest.json', '/icon-192.png', '/icon-512.png'])
+    )
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if(event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+
+  if(url.hostname === 'fonts.googleapis.com' || url.hostname === 'fonts.gstatic.com'){
+    event.respondWith(
+      caches.match(event.request).then(cached => cached ||
+        fetch(event.request).then(res => {
+          const clone = res.clone();
+          caches.open(CACHE_NAME).then(c => c.put(event.request, clone));
+          return res;
+        })
+      )
+    );
+    return;
+  }
+
+  // Network first for everything else
+  event.respondWith(
+    fetch(event.request)
+      .then(res => {
+        if(res && res.status === 200){
+          const clone = res.clone();
+          caches.open(CACHE_NAME).then(c => c.put(event.request, clone));
+        }
+        return res;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
