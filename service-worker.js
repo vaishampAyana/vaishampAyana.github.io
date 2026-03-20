6b65bb7798e4166fd4390b8b1f030d371689d345
vaishampAyana <hackerbachcha420@gmail.com>
2026-03-20 18:48:36 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pingala-chandas-v8';
+const CACHE_NAME = 'pingala-chandas-v9';
 
 self.addEventListener('message', event => {
   if(event.data?.type === 'SKIP_WAITING') self.skipWaiting();
